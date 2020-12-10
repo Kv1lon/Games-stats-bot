e8fcb032625dde7f71f910e266312c6b30d20cb9
0l1vka <hiphopd4@gmail.com>
2020-12-10 17:46:12 +0200

    dev; Added default structure

---
+++ bot.py
@@ -0,0 +1,43 @@
+import telebot
+
+bot= telebot.TeleBot('1360131281:AAFC1IwsXys5WrgtMfP4PhBxSdmvviGcDVM')
+keyboard = telebot.types.ReplyKeyboardMarkup(True,True)
+keyboard.row("/start")
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    user = message.from_user
+    if user.username == 'dodickovich' or user.username == 'DeSplit336':
+        bot.send_message(message.chat.id, 'Ты лох по жизни.<br><b> И я не кину тебе биологию</b>')
+    else:
+        IKeybord = telebot.types.InlineKeyboardMarkup()
+        FORTNITE = telebot.types.InlineKeyboardButton("Fortnite", callback_data="Fortnite")
+        APEX = telebot.types.InlineKeyboardButton("Apex Legends", callback_data="APEX")
+        CSGO = telebot.types.InlineKeyboardButton("CSGO", callback_data="CSGO")
+        VALORANT = telebot.types.InlineKeyboardButton("Valorant", callback_data="Valorant")
+        IKeybord.add(FORTNITE, CSGO, VALORANT, APEX)
+        bot.send_message(message.chat.id, 'Привет '+user.first_name+' , ты написал мне /start', reply_markup=IKeybord)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == 'Fortnite':
+                bot.send_message(call.message.chat.id, 'Fortnite')
+            elif call.data == 'APEX':
+                bot.send_message(call.message.chat.id, 'APEX')
+            elif call.data == 'CSGO':
+                bot.send_message(call.message.chat.id, 'CSGO')
+            elif call.data == 'Valorant':
+                bot.send_message(call.message.chat.id, 'Valorant')
+
+
+            # remove inline buttons
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Game:",
+                                  reply_markup=None)
+    except Exception as e:
+        print(repr(e))
+
+
+
+bot.polling()
