d21747168fce99acecfe4a6099ecc9b6282366d3
0l1vka <hiphopd4@gmail.com>
2020-12-15 10:26:10 +0200

    dev; chaged parser's framework

--- packege/parser.py
+++ packege/parser.py
@@ -1,21 +1,11 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
 
-def parse_fortnite(name):
-    URL='https://fortnitetracker.com/profile/all/'+name
-    HEADERS= {
-        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
-    }
-    response= requests.get(URL, headers = HEADERS)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    stats = soup.findAll('div', class_ = 'trn-card__content')
-    comps = []
+def main():
+    driver= webdriver.Chrome()
+    parser = None
 
-    for stat in stats:
-        comps.append({
-        })
-#        for comp in comps:
- #           print(comp)
-    return "sdsd"
+
+if __name__ == '__main__':
+    main()
 
 
